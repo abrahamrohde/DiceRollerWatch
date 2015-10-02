312bac4690214a650a5d7f86cd50f0f84b3a823f
Mike Litman <mike@awesomefat.com>
2015-10-02 14:32:58 -0500

    Initial Commit

--- DiceRoller WatchKit Extension/InterfaceController.swift
+++ DiceRoller WatchKit Extension/InterfaceController.swift
@@ -10,8 +10,40 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController
+{
 
+    let alert = WKAlertAction(title: "Ok", style: WKAlertActionStyle.Cancel, handler: { () -> Void in })
+        
+    @IBAction func d4ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%4 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    
+    @IBAction func d6ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%6 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    @IBAction func d8ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%8 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    @IBAction func d10ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%10 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    @IBAction func d12ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%12 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    @IBAction func d20ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%20 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
+    @IBAction func d100ButtonClicked()
+    {
+        self.presentAlertControllerWithTitle("The Roll", message: "\(rand()%100 + 1)", preferredStyle: WKAlertControllerStyle.Alert, actions: [alert])
+    }
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
